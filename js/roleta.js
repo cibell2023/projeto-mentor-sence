cebcc05703ba2ebdcdb30efad3ddcc1c8543e548
cibelly mariana <cibellymariana010@gmail.com>
2023-10-27 16:46:22 -0300

    atualizando projeto

--- js/roleta.js
+++ js/roleta.js
@@ -20,38 +20,71 @@ function funcaoGirar(){
 
   
 //lista de tarefas//
-  const inputElement = document.querySelector(".new-task-input");
-const addTaskButton = document.querySelector(".new-task-button");
-const tasksContainer = document.querySelector(".tasks-container");
-const listTasks = [];
-let id = 0;
+let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
 
-function addTask(){
-    const taskItemContainer = document.createElement("div");
-    taskItemContainer.classList.add("task-item");
+function addTarefa() {
+  //PEGAR O VALOR DIGITADO NO INPUT
+  let valorInput = input.value;
 
-   const taskContent = document.createElement("p");
-   taskContent.innerText = inputElement.value;
-   taskContent.id = id
-   
-   taskItemContainer.appendChild(taskContent);
+  //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
 
-   tasksContainer.appendChild(taskItemContainer);
-   if(inputElement.value != ""){
-    listTask.push(inputElement.value);
-   }
+    let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+    </div>`;
 
-   inputElement.value = "";
-   id++
+    //ADICIONAR NOVO ITEM NO MAIN
+    main.innerHTML += novoItem;
+
+    //ZERAR OS CAMPINHOS
+    input.value = "";
+    input.focus();
+  }
 }
 
-
-
-
-
-function toggleContrast() {
-  var formElement = document.getElementById("form")
-  formElement.className = "high-contrast";
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
 }
 
-  addTaskButton.addEventListener("click", () => addTask());
+function marcarTarefa(id) {
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
+  console.log(classe);
+
+  if (classe == "item") {
+    item.classList.add("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-circle-outline");
+    icone.classList.add("mdi-check-circle");
+
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-check-circle");
+    icone.classList.add("mdi-circle-outline");
+  }
+}
+
+input.addEventListener("keyup", function (event) {
+  //SE TECLOU ENTER (13)
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
